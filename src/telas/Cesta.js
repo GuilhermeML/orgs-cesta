fc328301485c8b6989e2965e8dd758ac50bb33ea
Guilherme Moura Leite <guilherme.mouraleite1@hotmail.com>
2022-08-31 09:17:43 -0300

    primeiro commit

---
+++ src/telas/Cesta.js
@@ -0,0 +1,29 @@
+import { Image, StyleSheet, Dimensions, Text } from 'react-native';
+
+import topo from "../../assets/topo.png";
+
+const width = Dimensions.get('screen').width;
+
+export default function Cesta() {
+    return <>
+        <Image source={topo} style={estilos.topo}/>
+        <Text style={estilos.titulo}>Detalhe da Cesta</Text>
+    </>
+}
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+    titulo: {
+        width: "100%",
+        position: "absolute", //relativa é uma apos a outra, absoluta é uma por cima da outra
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
+    }
+});
